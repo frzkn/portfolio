4efb73d8c39e26b22ee3ac2688709b98fcb5adbf
Faraz Khan <farazrk001@gmail.com>
2020-01-04 18:34:35 +0530

    Add page title

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,18 @@
 import React from "react"
+import { Helmet } from "react-helmet"
+
 import Hero from "../components/Hero"
 import Project from "../components/Project"
+
 export default () => {
   const [light, setLight] = React.useState(true)
   return (
     <div>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Faraz's Portfolio</title>
+      </Helmet>
       <Hero light={light} setLight={setLight} />
-
       <Project light={light} />
     </div>
   )
